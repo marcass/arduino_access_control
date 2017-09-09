931118b835a2c188db7cc62e34482c6f2c5bba1e
Marcass <best.nose@gmail.com>
2017-09-09 23:00:57 +1200

    trimmed down database stuff

--- python/keyserver.py
+++ python/keyserver.py
@@ -15,58 +15,57 @@
 # curl -X POST -H "Content-Type: application/json" -d '{"pincode":"000"}' http://127.0.0.1:5000/usekey
 # Response: {"pin_correct": False}
 
-# curl -X POST -H "Content-Type: application/json" -d '{"pincode":"0000"}' http://127.0.0.1:5000/usekey
+# curl -X POST -H "Content-Type: application/json" -d '{"pincode":"1111"}' http://127.0.0.1:5000/usekey
 # Response:  {"pin_correct": True}
 
-# curl -X POST -H "Content-Type: application/json" -d '{"pincode":"0000"}' http://127.0.0.1:5000/usekey
+# curl -X POST -H "Content-Type: application/json" -d '{"pincode":"1111"}' http://127.0.0.1:5000/usekey
 # Response:  {"pin_correct": False} # NOW FALSE because it was a burn code!
 
-# curl -X POST -H "Content-Type: application/json" -d '{"pincode":"00003","burnkey":"True"}' http://127.0.0.1:5000/addkey
+# curl -X POST -H "Content-Type: application/json" -d '{"username": "max", "keycode": "AAB23", "enabled":"1"}' http://127.0.0.1:5000/addkey
+# Response:  {  "Status": "Added key" }
+
+# curl -X POST -H "Content-Type: application/json" -d '{"username": "burner", "keycode": "1111", "enabled":"1"}' http://127.0.0.1:5000/addkey
 # Response:  {  "Status": "Added key" }
 
 #curl -X POST -H "Content-Type: application/json" -d '{"pincode":"00003"}' http://127.0.0.1:5000/usekey
 # Response:  {  "pin_correct": true}
 
-import users
+#curl -X PUT -H "Content-Type: application/json" -d '{"status":"open"}' http://127.0.0.1:5000/doorstatus
+# Response:  {  "doorstatus": "open"}
+
+
+import re
 import sql
 from flask import Flask, request, jsonify
 
-#ALWAYS_ALLOWED_KEYS_LIST = users.ALWAYS_ALLOWED_KEYS_LIST
-#BURN_KEYS_LIST = users.BURN_KEYS_LIST
-
 def get_allowed_keys():
-    global d
-    d = sql.build_user_dict_query()
-    print d
-    #{'username': [u'marcass'], 'name': [u'Marcus Wells'], 'keycode_en': [1], 'mqtt_id': [u'mqttdash-mi4c'], 'mqtt_id_en': [1], 'burner': [0], 'createtime': [datetime.datetime(2017, 9, 9, 0, 19, 4, 703059, tzinfo=<DstTzInfo 'Pacific/Auckland' NZST+12:00:00 STD>)], 'enabled': [1], 'keycode': [u'1234A']}
-    keys_buy_user = {a : d['username'] for a in d['keycode']}
-    print keys_by_user
-    
-    #add rfid if works
-    
-    #build list of allowed users
-    user_list = []
-    for i in d['enabled']:
-        if i == 1:
-            user_list.append(i['user_name'])
-            #test kecode enabled
-        
-    print user_list
-    allowed_keys_list = []
-    
-    #global ALWAYS_ALLOWED_KEYS_LIST, BURN_KEYS_LIST
-    #return ALWAYS_ALLOWED_KEYS_LIST+BURN_KEYS_LIST
+    d = sql.build_allowed_keys()
     return d
 
 def use_key(key):
-    global d
-    global ALWAYS_ALLOWED_KEYS_LIST, BURN_KEYS_LIST
-    if key in ALWAYS_ALLOWED_KEYS_LIST:
+    d = sql.build_allowed_keys()
+    if key in d['valid_keys']:
+        if key in d['burner']:
+            conn, c = sql.get_db()
+            #remove burnkey row
+            c.execute("UPDATE doorUsers SET enabled=? WHERE username=?", (0, 'burner'))
+            conn.commit()
+        print 'username = '+d['key_name'][key]
+        sql.insert_actionLog('Pinpad', key, d['key_name'][key])
         return True
-    elif key in BURN_KEYS_LIST:
-        BURN_KEYS_LIST.remove(key)
+    else:
+        sql.insert_actionLog('Pinpad', key)
+        return False
+
+def add_user_to_db(username, keycode, enabled, timeStart, timeEnd):
+    sql.setup_db()
+    d = sql.build_user_dict_query()
+    if (keycode in d['keycode']) or username in d['username']:
+        return False
+    else:
+        sql.add_user(username, keycode, enabled, timeStart, timeEnd)
         return True
-    return False
+
 
 def add_to_keys(key, typeburnkey=True):
     global ALWAYS_ALLOWED_KEYS_LIST, BURN_KEYS_LIST
@@ -74,7 +73,9 @@ def add_to_keys(key, typeburnkey=True):
         BURN_KEYS_LIST.append(key)
     else:
         ALWAYS_ALLOWED_KEYS_LIST.append(key)
+        
 
+sql.setup_db()
 app = Flask(__name__)
 
 @app.route("/")
@@ -88,21 +89,42 @@ def list_keys():
 
 @app.route("/usekey", methods=['POST',])
 def usekey():
-  content = request.get_json(silent=False)
-  pin = content['pincode']
-  if use_key(pin):
-    resp = {'pin_correct':True}
-  else:
-    resp = {'pin_correct':False}
-  return jsonify(resp), 200
+    content = request.get_json(silent=False)
+    pin = content['pincode']
+    if use_key(pin):
+        resp = {'pin_correct':True}
+    else:
+        resp = {'pin_correct':False}
+    return jsonify(resp), 200
 
 @app.route("/addkey", methods=['POST',])
 def addkey():
-  content = request.get_json(silent=False)
-  key = content['pincode']
-  if content.has_key('burnkey'):
-    add_to_keys(key, typeburnkey=content['burnkey'])
-  else:
-    add_to_keys(key, typeburnkey=True)
-  resp = {'Status':'Added key'}
-  return jsonify(resp), 200
+    content = request.get_json(silent=False)
+    #{"name": "pellets", "username":pell", "keycode":"00003","keycode_en": "1", "mqtt_id": "pell-guy", "mqtt_id_en":"0", "burner":"1", "enabled":"1"}
+    timeStart = None
+    timeEnd = None
+    if content.has_key('timeStart'):
+        timeStart = content['timeStart'] # parse this to datetime
+    if content.has_key('timeEnd'):
+        timeEnd = content['timeEnd'] # parse this to datetime
+    print content['keycode']
+    print len(content['keycode'])
+    if (len(content['keycode']) > 3) and (len(content['keycode']) < 11) and (re.match("^[A-D1-9]+$", content['keycode'])):
+        ret = add_user_to_db(content['username'], content['keycode'], int(content['enabled']), timeStart, timeEnd)
+        if ret:
+            resp = {'Status':'Added key for {}'.format(content['username'])}
+        else:
+            resp = {'Status':'Did not at key for {}. Either username or pin already in use'.format(content['username'])}
+            
+    else:
+        resp = {'Status':'Did not at key for {}. Pin must use numbers 1-9 and letters A-D and be at between 4 and 10 alphanumeric characters long'.format(content['username'])}
+    return jsonify(resp), 200
+
+@app.route("/doorstatus", methods=['PUT',])
+def update_status():
+    content = request.get_json(silent=False)
+    status = content["status"]
+    sql.update_doorstatus(status)
+    resp = {"doorstatus":status}
+    return jsonify(resp), 200
+    
--- python/sql.py
+++ python/sql.py
@@ -1,13 +1,13 @@
-import matplotlib
-matplotlib.use('Agg')
+#import matplotlib
+#matplotlib.use('Agg')
 import pytz
 import sys
 import time
 import datetime
 import sqlite3
-import matplotlib.pyplot as plt
-#from matplotlib.dates import DayLocator, HourLocator, DateFormatter, drange
-import matplotlib.dates as md
+#import matplotlib.pyplot as plt
+##from matplotlib.dates import DayLocator, HourLocator, DateFormatter, drange
+#import matplotlib.dates as md
 
 
 #sqlite stuff
@@ -24,23 +24,43 @@ def setup_db():
     # Create table
     conn, c = get_db()
     #type is of 'mqtt', 'keycode' or 'rfid'
-    c.execute('''CREATE TABLE IF NOT EXISTS doorAccess
-                    (acesstime TIMESTAMP, name TEXT, username TEXT, type TEXT)''')
-    conn.commit()
+    c.execute('''CREATE TABLE IF NOT EXISTS actionLog
+                    (actiontime TIMESTAMP, message TEXT, type TEXT)''')
     c.execute('''CREATE TABLE IF NOT EXISTS doorUsers
-                    (timestamp TIMESTAMP, name TEXT, username TEXT, keycode TEXT, keycode_en INTEGER, mqtt_id TEXT, mqtt_id_en INTEGER, burner INTEGER, enabled INTEGER )''')
+                    (username TEXT, keycode TEXT, enabled INTEGER, timeStart TIMESTAMP, timeEnd TIMESTAMP )''')
+    c.execute('''CREATE TABLE IF NOT EXISTS doorStates
+                    (timestamp TIMESTAMP, state TEXT )''')
     conn.commit() # Save (commit) the changes
 
+def update_doorstatus(status):
+    conn, c = get_db()
+    utcnow = datetime.datetime.utcnow()
+    c.execute("INSERT INTO doorStates VALUES (?,?)", (utcnow,status) )
+    conn.commit()
     
-def add_user(name, username, keycode, keycode_en, mqtt_id, mqtt_id_en, burner, enabled):
+def add_user(username, keycode, enabled, timeStart, timeEnd):
     # Insert a row of data
     conn, c = get_db()
-    c.execute("INSERT INTO doorUsers VALUES (?,?,?,?,?,?,?,?,?)", (datetime.datetime.utcnow(),name, username, keycode, keycode_en, mqtt_id, mqtt_id_en, burner, enabled) )
+    utcnow = datetime.datetime.utcnow()
+    if timeStart is None:
+        timeStart = utcnow
+    if timeEnd is None:
+        from dateutil.relativedelta import relativedelta
+        timeEnd = utcnow + relativedelta(years=20)
+    
+    c.execute("INSERT INTO doorUsers VALUES (?,?,?,?,?)",\
+        (username, keycode, enabled, timeStart,timeEnd) )
     conn.commit() # Save (commit) the changes
     
-def add_access(name, username, access_type):
+def insert_actionLog(access_type, pin=None, username='Someone'):
+    if username is not 'Someone':
+        pin = '****'
+    if pin is None:
+        pin = 'mqtt'
+    message = '{} attempted access with {}'.format(username, pin)
     conn, c = get_db()
-    c.execute("INSERT INTO doorAccess VALUES (?,?,?,?)", (datetime.datetime.utcnow(),name, username, access_type) )
+    c.execute("INSERT INTO actionLog VALUES (?,?,?)",\
+        (datetime.datetime.utcnow(), message, access_type) )
     conn.commit()
 
 def localtime_from_response(resp):
@@ -52,48 +72,35 @@ def build_user_dict_query():
     conn, c = get_db()
     c.execute("SELECT * FROM doorUsers")
     ret = c.fetchall()
-    createtime = [localtime_from_response(i[0]) for i in ret]
-    name = [i[1] for i in ret]
-    username = [i[2] for i in ret]
-    keycode = [i[3] for i in ret]
-    keycode_en = [i[4] for i in ret]
-    mqtt_id = [i[5] for i in ret]
-    mqtt_id_en = [i[6] for i in ret]
-    burner = [i[7] for i in ret]
-    enabled = [i[8] for i in ret]
-    ret_dict = {'createtime':createtime, 'name':name, 'username':username, 'keycode':keycode, 'keycode_en':keycode_en, 'mqtt_id':mqtt_id, 'mqtt_id_en':mqtt_id_en, 'burner':burner, 'enabled':enabled}
+    username = [i[0] for i in ret]
+    keycode = [i[1] for i in ret]
+    #enabled = [i[2] for i in ret]
+    #timeStart = [i[3] for i in ret]
+    #timeEnd = [i[4] for i in ret]
+    ret_dict = {'username':username, 'keycode':keycode}
     print ret_dict
     return ret_dict
 
 def build_allowed_keys():
+    utcnow = datetime.datetime.utcnow()
     conn, c = get_db()
-    c.execute("SELECT * FROM doorUsers")
+    c.execute("SELECT * FROM doorUsers WHERE ? BETWEEN timeStart AND timeEnd", (utcnow,))
     ret = c.fetchall()
-    name = [i[1] for i in ret]
-    username = [i[2] for i in ret]
-    keycode = [i[3] for i in ret]
-    keycode_en = [i[4] for i in ret]
-    mqtt_id = [i[5] for i in ret]
-    mqtt_id_en = [i[6] for i in ret]
-    burner = [i[7] for i in ret]
-    enabled = []
+    username = [i[0] for i in ret]
+    keycode = [i[1] for i in ret]
+    valid_keys = []
+    burner_key = []
+    key_user_list = {}
     for i in ret:
         #user enabled?
-        if i[8] == 1:
-            #keycode enabled?
-            if i[4] == 1:
-                enabled.append(i[3])
-    burner_key_list = []
-    burner_username = []
-    for a in ret:
-        #burner?
-        if a[7] == 1:
-            burner_key_list.append(a[3])
-            burner_username.append(a[2])
-    burner = {'keys':burner_key_list, 'username':burner_username}
-    print 'burner '+str(burner)
-    print 'enabled '+str(enabled)
-    ret_dict = {'enabled':enabled, 'burner':burner}
+        if i[2] == 1:
+            #build list of dicts
+            valid_keys.append(i[1])
+            key_user_list.update({i[1]:i[0]})
+        if i[0] == 'burner':
+            burner_key.append(i[1])
+    print 'valid_keys = '+str(valid_keys)
+    ret_dict = {'valid_keys':valid_keys, 'key_name':key_user_list, 'burner':burner_key}
     print ret_dict
     return ret_dict
     

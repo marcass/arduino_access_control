ef9957745ec9ac3314d6404138ae3139e53436bd
Marcass <best.nose@gmail.com>
2017-10-07 19:02:12 +1300

    added door status to python

--- python/mqtt.py
+++ python/mqtt.py
@@ -30,7 +30,8 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("doors/request/#", qos=2)
+    client.subscribe([("doors/request/#", 2), ("doors/status/#", 2)])
+    
     #client.subscribe("doors/requests/#")
 
 # The callback for when a PUBLISH message is received from the server.
@@ -38,7 +39,16 @@ def on_message(client, userdata, msg):
     print(msg.topic+' '+msg.payload)
     door = msg.topic.split('/')[-1]
     print 'Door is '+door
-    check_key(door, msg.payload)
+    if 'request' in msg.topic:
+        print 'Checking door key'
+        check_key(door, msg.payload)
+    if 'status' in msg.topic:
+        #publish status
+        status_dict = {0: 'Open', 1: 'Closed', 2:'Unknown'}
+        payload = {'door': door, 'status':status_dict[msg.payload]}
+        print 'Sending status update '+payload
+        r = requests.put(API_URL, json=payload)
+        print r.json()
 
 
 #subscribe to broker and test for messages below alert values

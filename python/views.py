10cc466980b814fb7ffa4fecaa54a5764d91783f
marcus <best.nose@gmail.com>
2017-11-03 13:06:23 +1300

    sorting routes for ketting keycode

--- python/views.py
+++ python/views.py
@@ -166,6 +166,17 @@ def get_user_role(username):
     print content
     return jsonify(sql.get_user_role(username)), 200
 
+@app.route("/user/data/<username>", methods=['GET',])
+@jwt_required
+def get_user_data(username):
+    '''
+    Receives: nothing
+    Returns {'username': max, 'role':'user'}
+    '''
+    content = request.get_json(silent=False)
+    print content
+    return jsonify(sql.fetch_user_data(username)), 200
+
 @app.route("/user", methods=['GET',])
 @jwt_required
 def get_user():

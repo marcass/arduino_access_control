7d1ecae8c985ab1c7a200db527a06d300bd6382a
Marcass <best.nose@gmail.com>
2018-08-06 19:41:43 +1200

    dinner time

--- arduino/temp_sensor_esp32/temp_sensor_esp32.ino
+++ arduino/temp_sensor_esp32/temp_sensor_esp32.ino
@@ -1,12 +1,14 @@
-//#include <WiFi.h>
-#include <WiFi101.h>
+#include <WiFi.h>
+//#include <WiFi101.h>
 #include <ArduinoHttpClient.h>
 #include "secrets.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <ArduinoJson.h>
 
 // Data wire is plugged into pin 2 on the Arduino
 #define ONE_WIRE_BUS 2
+OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 String sensorID = "hall";
@@ -14,33 +16,38 @@ String token;
 
 ///////please enter your sensitive data in the Secret tab/secrets.h
 /////// Wifi Settings ///////
-char ssid[] = SECRET_SSID;
-char pass[] = SECRET_PASS;
-char serverAddress[] = "192.168.0.3";  // server address
-int port = 8080;
+const char* ssid = SECRET_SSID;
+const char* password = SECRET_PASS;
+char serverAddress[] = API_SERVER;  // server address
+int port = API_PORT;
 
 WiFiClient wifi;
 HttpClient client = HttpClient(wifi, serverAddress, port);
 int status = WL_IDLE_STATUS;
 String response;
 int statusCode = 0;
+//Ensure token fits in here
+StaticJsonBuffer<200> jsonBuffer;
+
+void connectWifi(){
+  WiFi.begin (ssid, password);
+  Serial.print("Attempting to connect to Network named: ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+}
 
 void setup() {
   Serial.begin(9600);
-  while ( status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to Network named: ");
-    Serial.println(ssid);                   // print the network name (SSID);
-
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
-    // Start up the temperature measurement library
-    sensors.begin();
-  }
-
+  connectWifi();
+  // Start up the temperature measurement library
+  sensors.begin();
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
-
   // print your WiFi shield's IP address:
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
@@ -65,53 +72,71 @@ void getAuth() {
   Serial.println(response);
 //  parse jwt here
 //https://github.com/bblanchon/ArduinoJson
+  int len = response.length();
+  char json[len];
+  response.toCharArray(json, len);
+  JsonObject& root = jsonBuffer.parseObject(json);
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    return;
+  }
+  const char* jwt_token = root["access_token"];
+  token = String(jwt_token);
 }
 
 float temp() {
   Serial.print("Measuing temp...");
   sensors.requestTemperatures(); // Send the command to get temperatures
   Serial.println("DONE");
-//  temp = sensors.getTempCByIndex(0)
-  Serial.print("Temperature for Device 1 is: ");
-  Serial.print(temp); // Why "byIndex"? 
-    // You can have more than one IC on the same bus. 
-    // 0 refers to the first IC on the wire
-  return sensors.getTempCByIndex(0)
+  // Why "byIndex"? 
+  // You can have more than one IC on the same bus. 
+  // 0 refers to the first IC on the wire
+//  float intemp = sensors.getTempCByIndex(0);
+//  // Convert to string
+//  char buffer[12];
+//  return itoa(intemp,buffer,10); //(integer, yourBuffer, base)
+  return sensors.getTempCByIndex(0);
 }
 
-String updateAPI() {
+int updateAPI() {
   Serial.println("making GET request with HTTP basic authentication");
 //  build json
-//  char PostData[] = "{\"group\": \"julian\", \"sensor\": sensorID, \"temp\": temp}";
-  String postData = "{group: julian, sensor: "+sensorID+", temp:" +temp+"}";
+  char PostData[] = "{\"group\": \"julian\", \"sensor\": sensorID, \"temp\": temp}";
+  String postData = String(PostData);
+//  String postData = "{group: julian, sensor: "+sensorID+", temp:" +temp+"}";
 //  String contentType = "application/x-www-form-urlencoded";
   client.beginRequest();
   client.post("/data");
 //  client.sendHeader(HTTP_HEADER_CONTENT_TYPE, "application/x-www-form-urlencoded");
   client.sendHeader(HTTP_HEADER_CONTENT_TYPE, "application/json");
   client.sendHeader(HTTP_HEADER_CONTENT_LENGTH, postData.length());
+//  client.sendHeader(HTTP_HEADER_CONTENT_LENGTH, sizeof(postData));
   client.sendHeader("X-CUSTOM-HEADER", "Authorization: Bearer "+token);
 //  https://github.com/arduino-libraries/ArduinoHttpClient/blob/master/examples/CustomHeader/CustomHeader.ino up to here
 //  Creds in secrets.h
 //  client.sendBasicAuth(API_user, API_pass); // send the username and password for authentication
   client.endRequest();
   client.write((const byte*)postData.c_str(), postData.length());
+//  client.write((const byte*)postData.c_str(), sizeof(postData));
   // read the status code and body of the response
-  return client.responseStatusCode();
+  statusCode = client.responseStatusCode();
   response = client.responseBody();
   Serial.print("Status code: ");
   Serial.println(statusCode);
   Serial.print("Response: ");
   Serial.println(response);
   Serial.println("Wait five seconds");
+  return statusCode;
 }
 
 void loop() {
-  thisTemp = temp();
-  resp = updateAPI();
-  if (resp == "200") { //Consider testing for 401
+  float thisTemp = temp();
+  Serial.print("Temperature for Device 1 is: ");
+  Serial.print(thisTemp);
+  int resp = updateAPI();
+  if (resp == 200) { //Consider testing for 401
     Serial.println("Data successfully posted");
-  }else if (resp == "401"){
+  }else if (resp == 401){
     getAuth();
     resp = updateAPI();
   }

6fc07a87301eccbfb37574973f4687e35078ac24
marcus <best.nose@gmail.com>
2018-08-12 08:40:30 +1200

    another schema rejig

--- python/sensor_data.py
+++ python/sensor_data.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 import datetime
 import time
 
-sites = []
+measurement = []
 
 db_name = 'sensors'
 # setup reteniton plicy list must match index of durations
@@ -38,11 +38,11 @@ setup_db()
 client.switch_database(db_name)
 # client.drop_database(db_name)
 
-def setup_RP(site):
+def setup_RP(meas):
     global retention_policies
-    global sites
+    global measurement
     RP_list = []
-    site_rps = []
+    meas_rps = []
     try:
         RP = client.get_list_retention_policies(db_name)
         for i in RP:
@@ -51,39 +51,67 @@ def setup_RP(site):
     except:
         print 'No retention polices here'
     for i in retention_policies:
-        site_rps.append(site+'_'+i)
-    for i in site_rps:
-        val = site_rps.index(i)
+        meas_rps.append(meas+'_'+i)
+    for i in meas_rps:
+        val = meas_rps.index(i)
         if i not in RP_list:
             client.create_retention_policy(i, durations[val]['dur'], 1, database='sensors', default=durations[val]['default'])
     # https://influxdb-python.readthedocs.io/en/latest/api-documentation.html
     # https://docs.influxdata.com/influxdb/v1.6/guides/downsampling_and_retention/
     try:
-        client.query('CREATE CONTINUOUS QUERY \"%s\" ON %s BEGIN SELECT mean(temp) AS "temp", mean(humidity) AS "humidity", mean(light) AS "light" INTO \"%s\".values_7d FROM \"%s\" GROUP BY time(5m), * END' %(site+'_cq_7_days', db_name, site+'_7_days', site))
-        client.query('CREATE CONTINUOUS QUERY \"%s\" ON %s BEGIN SELECT mean(temp) AS "temp", mean(humidity) AS "humidity", mean(light) AS "light" INTO \"%s\".values_2mo FROM \"%s\" GROUP BY time(10m), * END' %(site+'_cq_2_months', db_name, site+'_2_months', site))
-        client.query('CREATE CONTINUOUS QUERY \"%s\" ON %s BEGIN SELECT mean(temp) AS "temp", mean(humidity) AS "humidity", mean(light) AS "light" INTO \"%s\".values_1y FROM \"%s\" GROUP BY time(20m), * END' %(site+'_cq_1_year', db_name, site+'_1_year', site))
-        client.query('CREATE CONTINUOUS QUERY \"%s\" ON %s BEGIN SELECT mean(temp) AS "temp", mean(humidity) AS "humidity", mean(light) AS "light" INTO \"%s\".values_5y FROM \"%s\" GROUP BY time(30m), * END' %(site+'_cq_5_years', db_name, site+'_5_years', site))
+        # client.query('CREATE CONTINUOUS QUERY \"%s\" ON %s BEGIN SELECT mean(temp) AS "temp", mean(humidity) AS "humidity", mean(light) AS "light" INTO \"%s\".values_7d FROM \"%s\" GROUP BY time(5m), * END' %(meas+'_cq_7_days', db_name, meas+'_7_days', meas))
+        # client.query('CREATE CONTINUOUS QUERY \"%s\" ON %s BEGIN SELECT mean(temp) AS "temp", mean(humidity) AS "humidity", mean(light) AS "light" INTO \"%s\".values_2mo FROM \"%s\" GROUP BY time(10m), * END' %(meas+'_cq_2_months', db_name, meas+'_2_months', meas))
+        # client.query('CREATE CONTINUOUS QUERY \"%s\" ON %s BEGIN SELECT mean(temp) AS "temp", mean(humidity) AS "humidity", mean(light) AS "light" INTO \"%s\".values_1y FROM \"%s\" GROUP BY time(20m), * END' %(meas+'_cq_1_year', db_name, meas+'_1_year', meas))
+        # client.query('CREATE CONTINUOUS QUERY \"%s\" ON %s BEGIN SELECT mean(temp) AS "temp", mean(humidity) AS "humidity", mean(light) AS "light" INTO \"%s\".values_5y FROM \"%s\" GROUP BY time(30m), * END' %(meas+'_cq_5_years', db_name, meas+'_5_years', meas))
+        client.query('CREATE CONTINUOUS QUERY \"%s\" ON %s BEGIN SELECT mean(value) AS "value" INTO \"%s\".values_7d FROM \"%s\" GROUP BY time(5m), * END' %(meas+'_cq_7_days', db_name, meas+'_7_days', meas))
+        client.query('CREATE CONTINUOUS QUERY \"%s\" ON %s BEGIN SELECT mean(value) AS "value" INTO \"%s\".values_2mo FROM \"%s\" GROUP BY time(10m), * END' %(meas+'_cq_2_months', db_name, meas+'_2_months', meas))
+        client.query('CREATE CONTINUOUS QUERY \"%s\" ON %s BEGIN SELECT mean(value) AS "value" INTO \"%s\".values_1y FROM \"%s\" GROUP BY time(20m), * END' %(meas+'_cq_1_year', db_name, meas+'_1_year', meas))
+        client.query('CREATE CONTINUOUS QUERY \"%s\" ON %s BEGIN SELECT mean(value) AS "value" INTO \"%s\".values_5y FROM \"%s\" GROUP BY time(30m), * END' %(meas+'_cq_5_years', db_name, meas+'_5_years', meas))
     except:
         # already exist
         print "Failed to create CQ for "+i+", as it already exists"
 
 # setup_RP()
 
+# def write_data(json):
+#     # ensure RP's and CQ's in place for new sites
+#     global sites
+#     if json['group'] not in sites:
+#         sites.append(json['group'])
+#         setup_RP(json['group'])
+#     try:
+#         json_data = [
+#             {
+#                 'measurement': json['group'],
+#                 'tags': {
+#                     'sensorID': json['sensor']
+#                 },
+#                 'fields': {
+#                     json['type']: float(json['value'])
+#                 },
+#                 'time': datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+#                 }
+#             ]
+#         client.write_points(json_data)
+#         return {'Status': 'success', 'Message': 'successfully wrote data points'}
+#     except:
+#         return {'Status': 'error', 'Messgage': 'failed to wrote data points'}
 def write_data(json):
     # ensure RP's and CQ's in place for new sites
-    global sites
-    if json['group'] not in sites:
-        sites.append(json['group'])
-        setup_RP(json['group'])
+    global measurement
+    if json['type'] not in measurement:
+        measurement.append(json['type'])
+        setup_RP(json['type'])
     try:
         json_data = [
             {
-                'measurement': json['group'],
+                'measurement': json['type'],
                 'tags': {
-                    'sensorID': json['sensor']
+                    'sensorID': json['sensor'],
+                    'site': json['group']
                 },
                 'fields': {
-                    json['type']: float(json['value'])
+                    json['type']: json['value']
                 },
                 'time': datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
                 }
@@ -94,40 +122,50 @@ def write_data(json):
         return {'Status': 'error', 'Messgage': 'failed to wrote data points'}
 
 def get_sensorIDs():
-    global sites
-    ret = []
-    for a in sites:
-        print a
-        res_dict = {'site': a, 'location': []}
-        # results = client.query('SHOW TAG VALUES ON "sensors" WITH KEY = sensorID')
-        # results = client.query('SHOW TAG VALUES ON "sensors" WITH KEY = sensorID WHERE "group" = \"%s\"' %(location))
-        results = client.query('SHOW TAG VALUES ON "sensors" FROM \"%s\" WITH KEY = sensorID' %(a))
-        locations = results.get_points()
-        # ids = []
-        # ret.append(res_dict)
-        for i in locations:
-            # this_dict = {'id': i['value'], 'fields': []}
-            this_res = client.query('SHOW FIELD KEYS ON "sensors" FROM \"%s\"' %(a))
-            sensors = this_res.get_points()
-            fields = [x['fieldKey'] for x in sensors]
-            this_dict = {'id': i['value'], 'fields': fields}
-            # for x in sensors:
-            #     fields =
-            #     this_dict['fields'].append(x['fieldKey'])
-            # ids.append(i['value'])
-            res_dict['location'].append(this_dict)
-        ret.append(res_dict)
-    print ret
-    # [{site: marcus, locatons: [{id: lounge, fields: []}, {id: kitchen, fields: []}]
-    # returns:
-    # [{'site': 'marcus', 'location': [{'fields': [u'light', u'temp'], 'id': u'downhall'}, {'fields': [u'light', u'temp'], 'id': u'kitchen'}, {'fields': [u'light', u'temp'], 'id': u'lounge'}, {'fields': [u'light', u'temp'], 'id': u'spare'}, {'fields': [u'light', u'temp'], 'id': u'window'}]}]
-    return ret
+    measurements = get_measurements()
+    meas = []
+    for i in measurements:
+        print i
+        results = client.query('SHOW TAG VALUES ON "sensors" FROM \"%s\" WITH KEY = site' %(i))
+        meas_types = results.get_points()
+        for a in meas_types:
+            print a
+
+# def get_sensorIDs1():
+#     global sites
+#     ret = []
+#     for a in sites:
+#         print a
+#         res_dict = {'site': a, 'location': []}
+#         # results = client.query('SHOW TAG VALUES ON "sensors" WITH KEY = sensorID')
+#         # results = client.query('SHOW TAG VALUES ON "sensors" WITH KEY = sensorID WHERE "group" = \"%s\"' %(location))
+#         results = client.query('SHOW TAG VALUES ON "sensors" FROM \"%s\" WITH KEY = sensorID' %(a))
+#         locations = results.get_points()
+#         # ids = []
+#         # ret.append(res_dict)
+#         for i in locations:
+#             # this_dict = {'id': i['value'], 'fields': []}
+#             this_res = client.query('SHOW FIELD KEYS ON "sensors" FROM \"%s\"' %(a))
+#             sensors = this_res.get_points()
+#             fields = [x['fieldKey'] for x in sensors]
+#             this_dict = {'id': i['value'], 'fields': fields}
+#             # for x in sensors:
+#             #     fields =
+#             #     this_dict['fields'].append(x['fieldKey'])
+#             # ids.append(i['value'])
+#             res_dict['location'].append(this_dict)
+#         ret.append(res_dict)
+#     print ret
+#     # [{site: marcus, locatons: [{id: lounge, fields: []}, {id: kitchen, fields: []}]
+#     # returns:
+#     # [{'site': 'marcus', 'location': [{'fields': [u'light', u'temp'], 'id': u'downhall'}, {'fields': [u'light', u'temp'], 'id': u'kitchen'}, {'fields': [u'light', u'temp'], 'id': u'lounge'}, {'fields': [u'light', u'temp'], 'id': u'spare'}, {'fields': [u'light', u'temp'], 'id': u'window'}]}]
+#     return ret
 
 def get_measurements():
     results = client.query('SHOW MEASUREMENTS ON "sensors"')
-    sites = results.get_points()
+    measurement = results.get_points()
     locs = []
-    for i in sites:
+    for i in measurement:
         locs.append(i['name'])
     return locs
 
